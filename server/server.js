d9fb8896e4746cd46ac8af9003365acaa637b33b
Kelly Copley <copleykj@gmail.com>
2018-03-27 11:33:26 -0400

    update server code

--- server/server.js
+++ server/server.js
@@ -1,37 +1,43 @@
+/* eslint-disable import/no-unresolved */
+import { LinkableModel } from 'meteor/socialize:linkable-model';
+/* eslint-enable import/no-unresolved */
+
+import { VotesCollection } from '../common/vote-model';
+
 VotesCollection.allow({
-    insert: function (userId, vote) {
+    insert(userId, vote) {
         return userId && vote.checkOwnership();
     },
-    remove: function (userId, vote) {
+    remove(userId, vote) {
         return userId && vote.checkOwnership();
-    }
+    },
 });
 
-VotesCollection.before.insert(function(userId, vote){
-    var currentVote = VotesCollection.findOne({userId:userId, linkedObjectId:vote.linkedObjectId});
-    if(currentVote){
-        if(vote.direction !== currentVote.direction){
-            currentVote.update({$set:{direction:vote.direction}});
-            return false;
-        }else{
+VotesCollection.before.insert(function beforeInsert(userId, vote) {
+    const currentVote = VotesCollection.findOne({ userId, linkedObjectId: vote.linkedObjectId });
+    if (currentVote) {
+        if (vote.direction !== currentVote.direction) {
+            currentVote.update({ $set: { direction: vote.direction } });
             return false;
         }
+        return false;
     }
+    return undefined;
 });
 
-VotesCollection.after.update(function (userId, vote) {
-    var collection = LinkableModel.getCollectionForRegisteredType(vote.objectType);
+VotesCollection.after.update(function afterUpdate(userId, vote) {
+    const collection = LinkableModel.getCollectionForRegisteredType(vote.objectType);
 
-    userId && collection && collection.update(vote.linkedObjectId, {$inc:{_voteScore:vote.direction*2}});
-}, {fetchPrevious:false});
+    userId && collection && collection.update(vote.linkedObjectId, { $inc: { _voteScore: vote.direction * 2 } });
+}, { fetchPrevious: false });
 
-VotesCollection.after.insert(function (userId, vote) {
-    var collection = LinkableModel.getCollectionForRegisteredType(vote.objectType);
+VotesCollection.after.insert(function afterInsert(userId, vote) {
+    const collection = LinkableModel.getCollectionForRegisteredType(vote.objectType);
 
-    userId && collection && collection.update(vote.linkedObjectId, {$inc:{_voteScore:vote.direction}});
+    userId && collection && collection.update(vote.linkedObjectId, { $inc: { _voteScore: vote.direction } });
 });
 
-VotesCollection.after.remove(function (userId, vote) {
-    var collection = LinkableModel.getCollectionForRegisteredType(vote.objectType);
-    userId && collection && collection.update(vote.linkedObjectId, {$inc:{_voteScore:-vote.direction}});
+VotesCollection.after.remove(function afterRemove(userId, vote) {
+    const collection = LinkableModel.getCollectionForRegisteredType(vote.objectType);
+    userId && collection && collection.update(vote.linkedObjectId, { $inc: { _voteScore: -vote.direction } });
 });
