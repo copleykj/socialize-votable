5b8c7fd840df493192227bbb357c11db46acaf05
Kelly Copley <copleykj@gmail.com>
2018-03-27 11:34:26 -0400

    update package.js to use modules

--- package.js
+++ package.js
@@ -1,21 +1,18 @@
+/* global Package */
 Package.describe({
-    name: "socialize:voteable",
-    summary: "A package for implementing social up/down style voting",
-    version: "0.1.0",
-    git: "https://github.com/copleykj/socialize-voteable.git"
+    name: 'socialize:voteable',
+    summary: 'A package for implementing social up/down style voting',
+    version: '1.0.0',
+    git: 'https://github.com/copleykj/socialize-voteable.git',
 });
 
-Package.onUse(function(api) {
-    api.versionsFrom("1.0.2.1");
+Package.onUse(function _(api) {
+    api.versionsFrom('1.3');
 
-    api.use("socialize:linkable-model@0.4.1");
+    api.use('socialize:linkable-model@1.0.1');
 
-    api.imply("socialize:linkable-model");
+    api.imply('socialize:linkable-model');
 
-    api.addFiles("common/voteable-model.js");
-    api.addFiles("common/vote-model.js");
-    api.addFiles("server/server.js", "server");
-
-
-    api.export(["VoteableModel", "Vote"]);
+    api.mainModule('server/server.js', 'server');
+    api.mainModule('common/common.js');
 });
