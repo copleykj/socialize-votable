b5b354630f3d84083232a3f6b437cbab6bdf8447
Kelly Copley <copleykj@gmail.com>
2018-05-27 09:23:40 -0400

    update deps

--- package.js
+++ package.js
@@ -10,9 +10,9 @@ Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
     api.use([
-        'socialize:linkable-model@1.0.1',
-        'socialize:user-blocking@1.0.0',
-        'reywood:publish-composite@1.5.2',
+        'socialize:linkable-model@1.0.3',
+        'socialize:user-blocking@1.0.1',
+        'reywood:publish-composite@1.6.0',
     ]);
 
     api.imply('socialize:linkable-model');
