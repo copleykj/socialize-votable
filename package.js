e59ddd7f6c0da7ab7a055655d55be12bb380b511
Kelly Copley <copleykj@gmail.com>
2020-08-17 09:16:31 -0400

    add meteorignore file

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
     name: 'socialize:voteable',
     summary: 'A package for implementing social up/down style voting',
-    version: '1.0.2',
+    version: '1.0.3',
     git: 'https://github.com/copleykj/socialize-voteable.git',
 });
 
@@ -10,8 +10,8 @@ Package.onUse(function _(api) {
     api.versionsFrom('1.10.2');
 
     api.use([
-        'socialize:linkable-model@1.0.4',
-        'socialize:user-blocking@1.0.4',
+        'socialize:linkable-model@1.0.5',
+        'socialize:user-blocking@1.0.5',
         'reywood:publish-composite@1.7.3',
     ]);
 
